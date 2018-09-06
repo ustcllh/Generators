eaf1f1d086e5c88bb1b753427ce486c932621be1
Lihan Liu <lihan.liu@vanderbilt.edu>
2018-09-06 10:12:38 -0500

    set path

--- setGeneratorsPath.sh
+++ setGeneratorsPath.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 export GENERATORSPATH=$PWD
+export PYTHIA8PATH="/cvmfs/cms.cern.ch/slc6_amd64_gcc700/external/pythia8/230-gnimlf4"
+export LHAPDF6PATH="/cvmfs/cms.cern.ch/slc6_amd64_gcc700/external/lhapdf/6.2.1"
+export FASTJETPATH="/cvmfs/cms.cern.ch/slc6_amd64_gcc700/external/fastjet/3.3.0"
